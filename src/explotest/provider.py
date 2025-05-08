ebeaa125def66e34b3e5e73c5b725d0be913365b
Kevin <kevinliu04@gmail.com>
2025-05-08 14:59:18 -0700

    feat: add stubs for provider.py

--- src/explotest/provider.py
+++ src/explotest/provider.py
@@ -1,2 +1,11 @@
-class Provider:
-    pass
+from abc import ABC, abstractmethod
+import ast
+
+
+class Provider(ABC):
+
+    @abstractmethod
+    def generate_active_ast(self) -> list[ast.AST]: ...
+
+    @abstractmethod
+    def generate_program_states(self) -> any: ...
--- src/explotest/system_provider.py
+++ src/explotest/system_provider.py
@@ -1,5 +1,12 @@
+import ast
+
 import provider
 
 
 class SystemProvider(provider.Provider):
-    pass
+
+    def generate_active_ast(self) -> list[ast.AST]:
+        return []
+
+    def generate_program_states(self) -> any:
+        return []
