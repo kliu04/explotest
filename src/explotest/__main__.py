3e4447160bc00d60ad35c8941651bd0ef2329fef
Kevin <kevinliu04@gmail.com>
2025-05-09 15:49:09 -0700

    refactor: clean up main++

--- src/explotest/__main__.py
+++ src/explotest/__main__.py
@@ -8,11 +8,12 @@ import os
 import inspect
 import re
 
-executed_lines = []
+
+executed_lines: list[str] = []
 
 
 # determine if a file is part of the standard library
-# E.g., /Library/Frameworks/Python.framework/Versions/3.13/lib/python3.13/pkgutil.py
+# E.g., /Library/Frameworks/Python.framework/Versions/3.13/lib/python3.13/...
 # TODO: check if OS independent
 def is_stdlib_file(filepath):
     stdlib_path = sysconfig.get_path("stdlib")
@@ -34,6 +35,7 @@ def tracer(frame: types.FrameType, event: str, arg: typing.Any):
 
     frame_info = inspect.getframeinfo(frame)
     filepath = frame_info.filename
+    positions = frame_info.positions
 
     # filter out python builtins
     if (
@@ -46,15 +48,14 @@ def tracer(frame: types.FrameType, event: str, arg: typing.Any):
 
     match event:
         case "call":
-            positions = frame_info.positions
             if positions and positions.lineno > 0:  # type: ignore
-                print(positions, filepath, "CALL")
+                print(positions, filepath, positions.lineno, "CALL")
                 executed_lines.append(*frame_info.code_context)
 
         case "line":
-            positions = frame_info.positions
-            print(positions, filepath)
-            executed_lines.append(*frame_info.code_context)
+            if positions:
+                print(positions, filepath, positions.lineno)
+                executed_lines.append(*frame_info.code_context)
 
         case "return":
             pass
@@ -82,10 +83,20 @@ def main():
     runpy.run_path(target, run_name="__main__")
     sys.settrace(None)
 
+    # FIXME?: recursively generate AST of imported modules (?)
+    # actually don't need to do this because we can call it :)
+    # unless that will mess up explore? not sure TBD
+    # since explore can be in multiple lines
+
+    # What formats to send to carver?
+    # AST optimization (do not load whole file)?
+    # can't use lineno/end_lineno trick since multi-line statements exist
+    # how to tell which things are executed?
+
     # generate AST of target
     target_ast = None
     with open(target, "r") as f:
-        target_ast = ast.parse(f.read())
+        target_ast = ast.parse(f.read(), filename=target)
 
     print(ast.dump(target_ast))
     print("".join(executed_lines))
