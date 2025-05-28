b52d443da2e056933728a873ea3026b185856e98
Kevin <kevinliu04@gmail.com>
2025-05-28 10:12:50 -0700

    docs: add some more comments

--- src/explotest/generated_test.py
+++ src/explotest/generated_test.py
@@ -6,9 +6,9 @@ from src.explotest.pytest_fixture import PyTestFixture
 
 @dataclass
 class GeneratedTest:
-    imports: list[ast.Import]
-    fixtures: list[PyTestFixture]
-    act_phase: ast.Assign
+    imports: list[ast.Import]  # needed imports for the test file
+    fixtures: list[PyTestFixture]  # argument generators
+    act_phase: ast.Assign  # calling the function-under-test
     asserts: list[ast.Assert]  # probably gonna be empty...
     definitions: list[ast.FunctionDef] # for REPL
 
