597f3615dd555092f15e03a5a382acce7ba37476
Kevin <kevinliu04@gmail.com>
2025-05-28 10:12:50 -0700

    docs: add some more comments

--- src/explotest/pytest_fixture.py
+++ src/explotest/pytest_fixture.py
@@ -5,9 +5,9 @@ from typing import Self
 
 @dataclass
 class PyTestFixture:
-    depends: list[Self]
-    parameter: str
-    body: list[
+    depends: list[Self]  # fixture dependencies
+    parameter: str  # parameter that this fixture generates
+    body: list[  # body of the fixture
         ast.AST
     ]  # TODO: find a way to guarantee that this fixture always returns.
 
