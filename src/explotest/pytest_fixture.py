274c4b42d5594ff0da9c3292b4a993920829caaf
Randy Zhu <randy@randyzhu.com>
2025-06-02 15:46:15 -0700

    idk

--- src/explotest/pytest_fixture.py
+++ src/explotest/pytest_fixture.py
@@ -21,3 +21,7 @@ class PyTestFixture:
         return ast.fix_missing_locations(ast.FunctionDef(name=f'generate_{self.parameter}', args=ast.arguments(
             args=[ast.arg(arg=f'generate_{dependency.parameter}') for dependency in self.depends]), body=self.body + [self.ret],
                                                          decorator_list=[pytest_deco]))
+
+
+    def __hash__(self) -> int:          # make the object usable as a dict key / set element
+        return hash(ast.unparse(self.ast_node))
