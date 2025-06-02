16b0cd01624c4ec14e99a4b0f4cd9144ac1b3c64
Randy Zhu <randy@randyzhu.com>
2025-06-02 14:00:33 -0700

    Modify testgen to support ipy frontend
    FIxed CICD

--- test_data/test_output.py
+++ test_data/test_output.py
@@ -1,16 +1,21 @@
 import dill
 import pytest
-import 
+from math import sin, pi
+import pandas as pd
+import numpy as np
+
+def tr_rule(f: pd.Series, x: pd.Series, dx: float, R: int):
+    return 2 / pi * dx * (1 / 2 * f.iloc[0] * sin(R * x.iloc[0]) + sum(f.iloc[1:-1] * (x.iloc[1:-1] * R).map(sin)) + 1 / 2 * f.iloc[-1] * sin(R * x.iloc[-1]))
 
 @pytest.fixture
 def generate_f():
-    with open('./pickled/f_5946fb37.pkl', 'rb') as f:
+    with open('./pickled/f_2305923b.pkl', 'rb') as f:
         f = dill.loads(f.read())
     return f
 
 @pytest.fixture
 def generate_x():
-    with open('./pickled/x_c8f470c2.pkl', 'rb') as f:
+    with open('./pickled/x_2e059836.pkl', 'rb') as f:
         x = dill.loads(f.read())
     return x
 
@@ -29,4 +34,4 @@ def test_tr_rule(generate_f, generate_x, generate_dx, generate_R):
     x = generate_x
     dx = generate_dx
     R = generate_R
-    return_value = .tr_rule(f, x, dx, R)
+    return_value = tr_rule(f, x, dx, R)
