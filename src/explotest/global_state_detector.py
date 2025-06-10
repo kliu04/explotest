8093f47e28905661fbc8ed8b47033bd34386f502
Randy Zhu <randy@randyzhu.com>
2025-06-10 13:47:37 -0700

    Add a submodule, E2E tests

---
+++ src/explotest/global_state_detector.py
@@ -0,0 +1,4 @@
+"""
+The global state detector detects all potential global state in a program. For now, the strategy is static in nature.
+It's a part of the test generation pass cycle. All global state will probably be mocked.
+"""
