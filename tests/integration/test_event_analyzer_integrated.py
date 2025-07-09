00938ecc0965f364b05c8dab21f83d10fb54bfd9
Randy Zhu <randy@randyzhu.com>
2025-07-09 14:28:13 -0700

    global state

---
+++ tests/integration/test_event_analyzer_integrated.py
@@ -0,0 +1,42 @@
+import pathlib
+
+import openai
+import pytest
+import pytest_mock
+from pytest_mock import MockerFixture
+
+import src.explotest.event_analyzer_for_global_state as eva
+import src.explotest.global_state_detector as gsd
+import ast
+
+import test_data.test_event_analyzer.simpleflask
+
+
+@pytest.fixture
+def oai() -> openai.OpenAI:
+    return openai.OpenAI(base_url=r"http://localhost:11434/v1", api_key=r"ollama")
+
+
+@pytest.fixture
+def analyzer(oai: openai.OpenAI) -> eva.EventAnalyzer:
+    p = pathlib.Path("../../test_data/test_event_analyzer/simpleflask.py")
+    sample_file_flask = p.read_text()
+    return eva.EventAnalyzer(
+        proc_filter=("hello_world", "./test_data/test_event_analyzer/simpleflask.py"),
+        capture_names_in_proc=[
+            ext.name
+            for ext in gsd.find_global_vars(ast.parse(sample_file_flask), "hello_world")
+        ],
+        oai=oai,
+    )
+
+
+def test_analyzer(analyzer: eva.EventAnalyzer, mocker: MockerFixture):
+    analyzer.start_tracking()
+    mock_req = mocker.MagicMock()
+    mock_req.method = "POST"
+    mocker.patch("test_data.test_event_analyzer.simpleflask.request", new=mock_req)
+    test_data.test_event_analyzer.simpleflask.hello_world()
+    result = analyzer.end_tracking()
+    print(result)
+    assert False
