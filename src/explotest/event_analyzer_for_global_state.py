a1daacc9f2c55594cdf2d2433e878afaf33190e6
Randy Zhu <randy@randyzhu.com>
2025-07-09 14:28:13 -0700

    global state

--- src/explotest/event_analyzer_for_global_state.py
+++ src/explotest/event_analyzer_for_global_state.py
@@ -6,20 +6,44 @@ Pass 2: Use an LLM to determine whether the external variable should be mocked
 Pass 3: Capture and store the value, creating mocks.
 """
 
+import inspect
 from sys import monitoring as sm
 from types import CodeType
 from typing import Any, Tuple
+
+import openai
 
 
 class EventAnalyzer:
     return_data: list[Tuple[CodeType, int, object]]
     line_data: list[Tuple[CodeType, int]]
+    proc_globals: list[object] = []
+    target_names_in_proc: list[str]
+    globals_by_frame_id: dict[int, dict[str, object]]
     TOOL_ID = 3
     TOOL_NAME = "explotest_mock_tracker"
+    llm: openai.OpenAI
 
-    def __init__(self):
+    def __init__(
+        self,
+        proc_filter: tuple[str, str],
+        capture_names_in_proc: list[str],
+        oai: openai.OpenAI,
+    ):
+        """
+        :param proc_filter is a tuple containing (in order) 1.: the function name and 2.: the file path of the function that `proc_filter` belongs to.
+
+        :param capture_names_in_proc are names we are interested inside the procedure - these have to be names.
+
+        :param oai openai.OpenAI API connection.
+        """
+        self.target_names_in_proc = capture_names_in_proc
+        self.globals_by_frame_id = {}
+        self.proc_filter = proc_filter
         self.return_data = []
         self.line_data = []
+
+        self.llm = oai
 
     def start_tracking(self):
         """
@@ -43,7 +67,26 @@ class EventAnalyzer:
     def return_handler(
         self, code: CodeType, instruction_offset: int, retval: object
     ) -> Any:
-        self.return_data.append((code, instruction_offset, retval))
+        # print(code.co_qualname)
+        if (
+            code.co_qualname
+            == self.proc_filter[0]
+            # and code.co_filename == self.proc_filter[1]
+        ):
+            current_frame = inspect.currentframe()
+            assert current_frame is not None
+            parent_frame = current_frame.f_back
+            assert parent_frame is not None
+            self.globals_by_frame_id[id(parent_frame)] = {}
+            for name, value in parent_frame.f_globals.items():
+                if (
+                    name not in self.globals_by_frame_id[id(parent_frame)]
+                    and name in self.target_names_in_proc
+                ):
+                    self.globals_by_frame_id[id(parent_frame)][name] = value
+            breakpoint()
+            self.return_data.append((code, instruction_offset, retval))
+
         return None
 
     def line_handler(self, code: CodeType, line_number: int) -> Any:
@@ -51,8 +94,9 @@ class EventAnalyzer:
         return None
 
     def end_tracking(self):
-        for code, instr_offset, retval in self.return_data:
-            print(code, instr_offset, retval)
-        for code, lineno in self.line_data:
-            print(code, lineno)
+        # for code, instr_offset, retval in self.return_data:
+        #     print(code, instr_offset, retval)
+        # for code, lineno in self.line_data:
+        #     print(code, lineno)
         sm.free_tool_id(self.TOOL_ID)
+        return self.return_data
--- tests/test_event_analyzer.py
+++ tests/test_event_analyzer.py
@@ -1,19 +1,29 @@
 from typing import Any, Generator
 
 import pytest
+import ast
 
 from explotest.event_analyzer_for_global_state import EventAnalyzer
 
 
 @pytest.fixture
 def analyzer():
-    eva = EventAnalyzer()
+    eva = EventAnalyzer("foo", [ast.Name(id="L", ctx=ast.Load())])
     eva.start_tracking()
     return eva
+
+
+L = []
+
+
+def foo():
+    L.append("meow")
+    return 1
 
 
 def test_one(analyzer):
     print("test")
     print("meow")
     assert True
-    analyzer.end_tracking()
+    foo()
+    print(analyzer.end_tracking())
