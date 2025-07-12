4268f21c9ca94c047dd7476ec5d37bb23207564c
Randy Zhu <randy@randyzhu.com>
2025-07-11 23:24:45 -0400

    miaow

--- tests/test_event_analyzer.py
+++ tests/test_event_analyzer.py
@@ -1,5 +1,6 @@
 from typing import Any, Generator
 
+import openai
 import pytest
 import ast
 
@@ -7,8 +8,13 @@ from explotest.event_analyzer_for_global_state import EventAnalyzer
 
 
 @pytest.fixture
-def analyzer():
-    eva = EventAnalyzer("foo", [ast.Name(id="L", ctx=ast.Load())])
+def oai() -> openai.OpenAI:
+    return openai.OpenAI(base_url=r"http://localhost:11434/v1", api_key=r"ollama")
+
+
+@pytest.fixture
+def analyzer(oai: openai.OpenAI):
+    eva = EventAnalyzer("foo", [ast.Name(id="L", ctx=ast.Load())], oai)
     eva.start_tracking()
     return eva
 
