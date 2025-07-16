d0b5dfec8a08c91ad3b49befa72e74ba222aa28e
Kevin <kevinliu04@gmail.com>
2025-07-16 14:37:42 -0700

    feat: importlib stuff

--- src/explotest/__main__.py
+++ src/explotest/__main__.py
@@ -4,8 +4,8 @@ Sets up tracer that will track every executed line
 """
 
 import ast
+import importlib
 import os
-import runpy
 import sys
 import types
 from pathlib import Path
@@ -45,7 +45,7 @@ def tracer(frame: types.FrameType, event, arg):
             t = tracked_files[path]
         else:
             tree = ast.parse(source, filename=path.name)
-            t = ASTFile(tree)
+            t = ASTFile(path.name, tree)
             tracked_files[path] = t
 
         if event == "line":
@@ -56,20 +56,97 @@ def tracer(frame: types.FrameType, event, arg):
     return tracer
 
 
+class Transformer(ast.NodeTransformer):
+
+    def visit_Assign(self, node):
+        return [node, ast.Pass()]
+
+
+from importlib.util import spec_from_file_location
+import importlib.abc
+
+
+class Loader(importlib.abc.Loader):
+    """Implement importlib.Loader"""
+
+    def __init__(self, run_as_main):
+        self.run_as_main = run_as_main
+
+    def exec_module(self, module):
+
+        with open(module.__file__) as f:
+            src = f.read()
+
+        tree = ast.parse(src, module.__file__, "exec")
+        trans = Transformer()
+        patched_tree = trans.visit(tree)
+        ast.fix_missing_locations(patched_tree)
+
+        code = compile(patched_tree, module.__file__, "exec")
+
+        if self.run_as_main:
+            module.__dict__["__name__"] = "__main__"
+            self.run_as_main = False
+        exec(code, module.__dict__)
+
+    def create_module(self, spec):
+        return None  # Use default module creation
+
+
+class Finder(importlib.abc.MetaPathFinder):
+    """An importlib finder that will handler files from user code directory."""
+
+    def __init__(self, code_dir):
+        self.code_dir = code_dir
+        self.run_as_main = True
+
+    def find_spec(self, fullname: str, path: list[str], target=None):
+        if path:
+            mod_path = Path(path[0])
+            if not mod_path.is_relative_to(self.code_dir):
+                return None
+        relative_path = fullname.replace(".", "/")  # json.decoder -> json/decoder
+        # NOTE: We currently don't support packages (directory with __init__.py)
+        # We'll consider that once there's a concrete user request
+        full_path = self.code_dir / (relative_path + ".py")
+        if not full_path.is_file():
+            return None
+
+        loader = Loader(self.run_as_main)
+        self.run_as_main = False
+        spec = spec_from_file_location(fullname, full_path, loader=loader)
+        return spec
+
+
+def load_code(root_path: Path, module_name: str):
+    """Load user code, patch function calls."""
+    finder = Finder(root_path)
+    try:
+        sys.meta_path.insert(0, finder)
+        module_name = module_name.replace(".py", "")
+        # module_name = "sherlock_project"
+        module_name = "scratchpad"
+        return importlib.import_module(module_name)
+    finally:
+        sys.meta_path.pop(0)
+
+
 def main():
     if len(sys.argv) < 2:
         print("Usage: python3 -m explotest <filename>")
         sys.exit(1)
 
     target = sys.argv[1]
+    target_folder = os.path.dirname(target)
     sys.argv = sys.argv[1:]
 
-    script_dir = os.path.abspath(os.path.dirname(target))
+    script_dir = os.path.abspath(target_folder)
     sys.path.insert(0, script_dir)
 
     # TODO: make this work for modules
     sys.settrace(tracer)
-    runpy.run_path(os.path.abspath(target), run_name="__main__")
+    load_code(Path(script_dir), target_folder)
+    # runpy.run_path(os.path.abspath(target), run_name="__main__")
     sys.settrace(None)
 
     for tf in tracked_files.values():
--- src/explotest/ast_file.py
+++ src/explotest/ast_file.py
@@ -4,11 +4,13 @@ from dataclasses import dataclass
 
 @dataclass
 class ASTFile:
+    filename: str
     nodes: ast.Module
     traced_line_numbers: set[int]
     line_comments: list[int]
 
-    def __init__(self, nodes):
+    def __init__(self, filename, nodes):
+        self.filename = filename
         self.nodes = nodes
         self.traced_line_numbers = set()
         self.line_comments = list()
