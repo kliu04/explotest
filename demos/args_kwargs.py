7d43c2f522c54405475f35801796fce71ba350e2
Kevin <kevinliu04@gmail.com>
2025-10-20 21:10:51 -0700

    fix: add demo with '/' and '*'

--- demos/args_kwargs.py
+++ demos/args_kwargs.py
@@ -10,7 +10,15 @@ def foo1(x, y, z, *args, bar, baz, **kwargs):
 def foo2(x, y, z=0):
     print(x, y, z)
 
+@explotest.explore(mode="p")
+def bar(x, y, /, z, *, bar):
+    print(x, y, z, bar)
+
+bar(1, 2, 3, bar=4)
+
 
 foo1(1, 2, 3, 4, 5, baz=6, bar=7, kwarg1=True, kwarg2=False)
 foo2(1, 2, 3)
 foo2(1, 2)
+bar(1,2, 3, bar=4)
+
