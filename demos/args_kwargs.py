bdeffb1f34a6e9818f52d7dd04cda952312c1914
Kevin <kevinliu04@gmail.com>
2025-10-20 21:11:02 -0700

    chore: whitespace

--- demos/args_kwargs.py
+++ demos/args_kwargs.py
@@ -10,9 +10,11 @@ def foo1(x, y, z, *args, bar, baz, **kwargs):
 def foo2(x, y, z=0):
     print(x, y, z)
 
+
 @explotest.explore(mode="p")
 def bar(x, y, /, z, *, bar):
     print(x, y, z, bar)
+
 
 bar(1, 2, 3, bar=4)
 
@@ -20,5 +22,4 @@ bar(1, 2, 3, bar=4)
 foo1(1, 2, 3, 4, 5, baz=6, bar=7, kwarg1=True, kwarg2=False)
 foo2(1, 2, 3)
 foo2(1, 2)
-bar(1,2, 3, bar=4)
-
+bar(1, 2, 3, bar=4)
